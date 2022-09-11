56d8dd0fbcdb5a94c74d97cb9699c7b1048070db
ARV <arief0303@gmail.com>
2022-09-11 12:32:50 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Init</h1>
+      <UilReact />
     </div>
   );
 }
