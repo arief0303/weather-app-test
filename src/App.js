51d9561e6257617e6ee1d9ec79993cc5f61577cf
ARV <arief0303@gmail.com>
2022-09-12 19:23:56 +0700

    ver1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import "./App.css";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [query, setQuery] = useState({ q: "Bandung" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
   return (
-    <div className="App">
-      <h1>Init</h1>
-      <UilReact />
+    <div
+      className={`mx-auto max-w-screen-md my-4 py-5 px-32 pb-14 drop-shadow-xl rounded-3xl bg-gradient-to-r from-cyan-500 to-blue-500 h-fit shadow-xl shadow-gray-400`}
+    >
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
+
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
     </div>
   );
 }
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
+import { toast } from "react-toastify";
+
+function Input({ setQuery, units, setUnits }) {
+    const [city, setCity] = useState("");
+  
+    const handleSearchClick = () => {
+      if (city !== "") setQuery({ q: city });
+    };
+  
+    const handleLocationClick = () => {
+        if (navigator.geolocation) {
+          toast.info("Fetching users location.");
+          navigator.geolocation.getCurrentPosition((position) => {
+            toast.success("Location fetched!");
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+    
+            setQuery({
+              lat,
+              lon,
+            });
+          });
+        }
+      };
+
+      const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          if (city !== "") setQuery({ q: city });
+        }
+      }
+    
+      return (
+        <div className="flex flex-row justify-center my-6">
+          <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+            <input
+              onKeyDown={handleKeyDown}
+              value={city}
+              onChange={(e) => setCity(e.currentTarget.value)}
+              type="text"
+              placeholder="Search for city..."
+              className="text-xl font-light p-2 px-6 w-full rounded-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+            />
+            <UilSearch
+              size={25}
+              className="text-white cursor-pointer transition ease-out hover:scale-125"
+              onClick={handleSearchClick}
+            />
+            <UilLocationPoint
+              size={25}
+              className="text-white cursor-pointer transition ease-out hover:scale-125"
+              onClick={handleLocationClick}
+            />
+          </div>
+        </div>
+      );    
+}
+
+export default Input
